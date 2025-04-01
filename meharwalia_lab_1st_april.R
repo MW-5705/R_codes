2139ce9b3b941e43b2f7ed09e78ae91511edf414
Mehar Walia <113458487+MW-5705@users.noreply.github.com>
2025-04-01 15:29:39 +0530

    Add files via upload

---
+++ meharwalia_lab_1st_april.R
@@ -0,0 +1,33 @@
+res = punif(45, 0, 60)
+
+1-res
+
+r30 = punif(30, 0, 60)
+r20 = punif(20, 0, 60)
+
+r30-r20
+
+
+dexp(3,1/2)
+
+x = seq(0,5,0.5)
+
+f = dexp(x, 1/2)
+
+plot(x,f)
+
+f = pexp(x, 1/2)
+
+res_3 = pexp(3,1/2)
+
+ans = res_3
+ans
+
+res_4 = rexp(1000, 0.5)
+plot(res_4)
+
+dgamma(3, 2, 1/3)
+
+pgamma(3, 2, 1/3)
+
+qgamma(0.70, 2, 1/3)
